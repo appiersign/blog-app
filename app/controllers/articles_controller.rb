383ff5b6e50a9e8a470bed456c604e37132e082a
Solomon APPIER-SIGN <braasig@gmail.com>
2020-08-10 23:05:05 +0000

    added new and create methods to handle create form and store

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,19 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(
+      title: params[:article][:title],
+      body: params[:article][:body]
+    )
+    @article.save
+    redirect_to(@article)
+  end
+
   def show
     @article = Article.find(params[:id])
   end
